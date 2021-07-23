6feca37de2b096c58e4b86ac465d4322352d5dd3
Justin Johnson <jj@Justins-MacBook-Pro.local>
2021-07-22 21:29:56 -0600

    Revert "Converted class attributes to properties"
    This reverts commit 6dea67079ef4f770bad40af8911741f656f1d5fc.

--- glitchfx2/__main__.py
+++ glitchfx2/__main__.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         # choose a file already in the src folder
         fileIn = Prompt().getInputFile()
         parser = ImageInputParser(fileIn)
-        (f, s) = (parser.getFileIn(), parser.getScale())
+        (f, s) = (parser.fileIn, parser.scale)
         # pass the absolute path into GlitchFX: this will be the read image
         gfx = GlitchFX(os.path.join(os.getcwd(), f), scale=s)
         print(gfx)
@@ -35,4 +35,3 @@ if __name__ == "__main__":
             imwrite(destFileName, gfx.dest)
             print("Sucessfully saved to " + os.path.join(os.getcwd(), destFileName))
         del gfx
-        print("Done")
---
+++ glitchfx2/initialize/gfx.py
@@ -0,0 +1,42 @@
+import cv2 as cv
+from util.argparser import ArgParser
+from util.prompts import Prompt
+from filters.fxdictionary import effects
+
+
+class GlitchFX:
+    def __init__(self, imagePath, scale=1.0):
+        self.parser = ArgParser()
+        self.srcPath = imagePath
+        self.src = cv.imread(self.srcPath)
+        (w, h) = (self.src.shape[1], self.src.shape[0])
+        newSize = (int(w * scale), int(h * scale))
+        self.src = cv.resize(self.src, newSize, cv.INTER_CUBIC)
+        self.dest = self.src
+
+    def __str__(self):
+        (w, h) = (self.src.shape[1], self.src.shape[0])
+        return f"\nNew GlitchFX\nSource: {self.srcPath}\nW:{w}px, H:{h}px"
+
+    def __del__(self):
+        print("Done")
+
+    def glitch(self, req, srcImg, **kwargs):
+        try:
+            self.dest = effects[req](srcImg, **kwargs)
+        except KeyError:
+            raise Exception("invalid effect")
+
+    def applyEffects(self):
+        nextFx = Prompt().enterEffect()
+        while nextFx != "x":
+            self.parser.getInputArray(nextFx)
+            self.parser.parse()
+            self.glitch(self.parser.effect, self.dest, **self.parser.argDict)
+            nextFx = Prompt().enterEffect()
+
+    def displaySrc(self):
+        cv.imshow("Glitch FX src", self.src)
+
+    def displayDst(self):
+        cv.imshow("Glitch FX dst", self.dest)
